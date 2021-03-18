475380f9693790115dbb4399dcfd1e3f2c1cae3b
Aldosk <Aldosk@users.noreply.github.com>
2021-03-18 00:39:07 -0500

    Added more comments

--- Client_One.py
+++ Client_One.py
@@ -4,36 +4,38 @@ import pickle
 import threading
 import time
 
+
+# The message receiving thread
 def msgRecv():
-    while  True:
+    while True:
+        # The receiving TCP socket
         dataClient = (clientSocket.recv(4096))
         # Handle object data
         try:
-            # Unpickle data
+            # Unpickle data - convert the object byte stream to object
             dataMsg = pickle.loads(dataClient)
             print('Thread Recv a msg')
 
             print(str(dataMsg.msgBody) + '\n')
-			 # Temporery use talk end command from the other client 
-             #to end the read and exit so it does not run in the background
+            # Temporaryly will use 'talk end' command from the other client
+            # to end the read and exit so it does not run in the background
             if dataMsg.msgBody == 'end talk':
                 print('Exit thread break')
                 break
 
-        # Handle normal data type  data
+        # Handle normal data type  data - this will used to exit the client once the server shutdown
         except:
-            print('except break')
+            print('Except break')
             break
 
 
-
-
-
-
+# Client Title
 print(" CLient 1 ")
-var = MessageObject("", -1, "", -1) # MsgType, senderId, msgBody, targetId
 
-clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+var = MessageObject("", -1, "", -1)  # MsgType, senderId, msgBody, targetId
+# Create a TCP socket
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Create a UDP socket
 udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 try:
@@ -41,25 +43,28 @@ try:
 except socket.error as e:
     print(str(e))
 
-# Send in the id
+# ====================================================
+# The Client TCP section - Mixed for now
+# ====================================================
+# Send in the id of the client to the server
 var.msgType = 'HELLO'
 var.senderId = 111
 senderKey = 100
-# Send data to the server
+# Pickle convert the object into a byte stream
 dataObject = pickle.dumps(var)
-
 
 msgTargetId = -1
 udpConnect = True
 reply = None
+
 while True:
-    #Initiation Phase
+    # Initiation Phase
     if udpConnect:
         try:
             if reply == None:
-                udpSocket.send(str.encode("HELLO "+str(var.senderId)))
+                udpSocket.send(str.encode("HELLO " + str(var.senderId)))
             elif reply != [] and reply[0] == "CHALLENGE":
-                udpSocket.send(str.encode("RESPONSE "+str(senderKey)))
+                udpSocket.send(str.encode("RESPONSE " + str(senderKey)))
             elif reply != [] and reply[0] == "AUTH_FAIL":
                 reply = None
                 udpSocket.close()
@@ -72,6 +77,7 @@ while True:
                 except socket.error as e:
                     print(str(e))
                 clientSocket.send(dataObject)
+                # Start the thread to receive message with non blocking type
                 threading.Thread(target=msgRecv).start()
             if udpConnect:
                 # Time out period
@@ -83,33 +89,55 @@ while True:
             # Assume Packet lost try again
             reply = None
             print("Time Out")
-            #udpSocket.close()
+            # udpSocket.close()
             break
     else:
         # CHAT PHASE
+        # Ask user for input
         msgInput = input("Client One Msg : \n")
+
+        # If chat message start with the word chat , it indicate that the client is making a chat request to a target
+        # client. Currently both clients need to target each other using the chat command
+        # chat [target_id_number]
+
         if msgInput.split()[0] == 'chat':
+            # The sender id of this client
             var.senderId = 111
+            # The target id of the client
             var.targetId = msgInput.split()[1]
-            msgTargetId  = msgInput.split()[1]
+            # Set the target id based on the value following the chat keyword
+            # chat [target_id_number]
+            msgTargetId = msgInput.split()[1]
+            # Message type so the server set up the target client
             var.msgType = 'CHATSET'
             var.msgBody = msgInput
             dataObject = pickle.dumps(var)
             # Send data to the server
             clientSocket.send(dataObject)
+
+        # Temp test msg - 'end client' could be used is temp place holder to end thread
+        # but 'end talk' will be used to end the client
         elif msgInput != 'end client':
             print('---------')
+            # Set the object data fields
+            # client sender id
             var.senderId = 111
+            # message type
             var.msgType = 'MSG'
+            # message body that based on user input
             var.msgBody = msgInput
+            # the target id which is based on using chat [target_id_number] command beforehand
             var.targetId = msgTargetId
+            # Pickle convert the object into a byte stream
             dataObject = pickle.dumps(var)
             # Send data to the server
             clientSocket.send(dataObject)
 
-
+        # Send the command to end server
         else:
             msgInput = 'end server'
-            clientSocket.send(bytes(msgInput,'utf-8'))
+            clientSocket.send(bytes(msgInput, 'utf-8'))
             break
+
+# Close socket
 clientSocket.close()
--- Server_TCP.py
+++ Server_TCP.py
@@ -6,13 +6,14 @@ import queue
 from MessageObject import MessageObject
 
 print("Server Runing")
-
-serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-udpSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-#serverSocket.set_reuse_addr()
+# Create the server TCP socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# serverSocket.set_reuse_addr()
 serverSocket.setblocking(False)
 
 try:
+    # Binds the TCP socket
     serverSocket.bind(('localhost', 6265))
 except socket.error as e:
     print(str(e))
@@ -21,11 +22,12 @@ try:
 except socket.error as e:
     print(str(e))
 print(serverSocket)
-# Make this a  server
 
 # UDP
-clients = {111:100, 222:200, 333:300} #temp
+clients = {111: 100, 222: 200, 333: 300}  # temp
 addrToId = {}
+
+
 def udp_thread():
     while True:
         resp = ""
@@ -35,7 +37,7 @@ def udp_thread():
         if data[0] == "HELLO":
             if int(data[1]) in clients:
                 addrToId[addr] = int(data[1])
-                resp = "CHALLENGE "+str(123)
+                resp = "CHALLENGE " + str(123)
             else:
                 # failed challenge
                 del addrToId[addr]
@@ -45,121 +47,182 @@ def udp_thread():
             else:
                 del addrToId[addr]
                 resp = "AUTH_FAIL"
-        udpSocket.sendto(str.encode(resp),addr)
+        udpSocket.sendto(str.encode(resp), addr)
     udpSocket.close()
+
+
 threading.Thread(target=udp_thread).start()
 
-# TCP
-serverSocket.listen(4)
+# ====================================================
+# The server TCP section
+# ====================================================
+# The TCP server will listen to up to 8 connection
+# This for the TCP  server socket
+serverSocket.listen(8)
 
+# A list that will contain all curently client connected socket
 listOfClientSocketOnline = []
+# A list that will contain all currently client connected id
 listOfClientsOnlineId = []
+# A list that will contains all the clients that are currently talking to each other - not used for now
 connectedPair = []
+
+# potential_readers , potential_writes, potential_errors that will be passed to the select
+# potential_readers is a list of all the sockets that will be ready to read from
 potential_readers = [serverSocket]
+# potential_writes is a list of all the socket  that will be ready to write to
 potential_writes = []
+# potential_errors is a lsit that will contain error from handling socket - currently does nothing might be used later
 potential_errors = []
+# A queue that will hold the message
 ClientMessageQueue = {}
+
+# Currently The server will keep on running until it 'end server' command is used by one of the clients
 while True:
-        #  Select 
-        select_ready_to_read , select_ready_to_write, select_error = select.select(potential_readers,potential_writes,potential_errors)
-        for socketTypesRead in select_ready_to_read:
-            if socketTypesRead is serverSocket:
-                # Accepts Clients Chat Phase
-                connectionSocket,connectionAddress = serverSocket.accept()
-                print(connectionSocket)
-                print(connectionAddress)
-                # Prevent connection from blocking
-                connectionSocket.setblocking(False)
-                # Connection Will each have a queue
-                potential_readers.append(connectionSocket)
 
-                listOfClientSocketOnline.append(connectionSocket)
-                ClientMessageQueue[connectionSocket] = queue.Queue()
-            else:
-                msgObject = socketTypesRead.recv(4096)
-                msgObjectDecoded = pickle.loads(msgObject)
+    # Select function will have potential_readers , potential_writes, potential_errors as parameters where each are
+    # lists that will wait until they are ready for reading incoming sockets from client, writing to clients sockets,
+    # and errors are received and need to be checked. The select function will provide a multiplexer like behavior to be
+    # able send data between clients
 
-                print(msgObjectDecoded)
-                #decodeMsg = msgObject.decode('utf-8')
-                # Add to connected client list
-                if msgObjectDecoded.msgType == 'HELLO' :
-                   print("HELLO MsgType")
-                   # Save the client id to list
+    # Can add the UDP socket to the select ?
 
-                   listOfClientsOnlineId.append(msgObjectDecoded.senderId)
-                   print(listOfClientsOnlineId)
-                   continue
-                if msgObjectDecoded.msgType == 'CHATSET' :
-                   print("CHATSET MsgType")
-                   print("Connect to")
-                   print(msgObjectDecoded.targetId)
-                   print(listOfClientsOnlineId)
-                   # Assume that target is already online
-                   # We can add a check if needed
-                   # Can Check also if already connected
-                   connectedPair.append(tuple((int(msgObjectDecoded.targetId), msgObjectDecoded.senderId)))
-                   continue
-                if msgObjectDecoded.msgBody == 'end talk':
-                    print(msgObject)
-                    #close the socket of the ccurrent connection
-                    #close the other client socket
-                    #break
-                    # end client threads
-                    socketTypesRead.send(bytes("end talk",'utf-8'))
+    # select_ready_to_read, select_ready_to_write, select_error are return result of the select each will contain
+    # sockets that are readable, writable and containing errors respectively
+    select_ready_to_read, select_ready_to_write, select_error = select.select(potential_readers, potential_writes,
+                                                                              potential_errors)
 
-                if msgObjectDecoded.msgBody == 'end server':
-                    print(msgObject)
-                    print(listOfClientSocketOnline)
-                    #close all socket of the ccurrent connection
-                    for conections in listOfClientSocketOnline:
-                         # Inform the clients of the closure
-                         conections.close()
-                    # end all clients threads
-                    # ClientMessageQueue[connectionSocket]close ?
-                    serverSocket.close()
-                    exit(0)
-                    #break
-                # Valid
+    # For each socket that is received and is in the select_ready_to_read
+    for socketTypesRead in select_ready_to_read:
 
-                # behave like a message
-                if msgObject: # might be none
-                    ClientMessageQueue[socketTypesRead].put(msgObject)
-                    # Valid
-                    if socketTypesRead not in potential_writes:
-                        potential_writes.append(socketTypesRead)
-                # none msgObject
-                else:
-                    if socketTypesRead  in potential_writes:
-                        potential_writes.remove(socketTypesRead)
+        # Read from the server socket
+        if socketTypesRead is serverSocket:
+            # Accepts Clients for the Chat Phase
+            connectionSocket, connectionAddress = serverSocket.accept()
+            print(connectionSocket)
+            print(connectionAddress)
+            # Prevent connection from blocking
+            connectionSocket.setblocking(False)
 
-                    potential_readers.remove(socketTypesRead)
-                    socketTypesRead.close()
-                    del ClientMessageQueue[socketTypesRead]
-        for socketTypesWrite in select_ready_to_write:
-            try:
-                msgOn = ClientMessageQueue[socketTypesWrite].get_nowait()
-            except queue.Empty:
-                potential_writes.remove(socketTypesWrite)
+            # Connection Will each have a queue
+            potential_readers.append(connectionSocket)
+            # Add the new client to the list of connected clients
+            # This list will contain all the clients that have connected client to the server
+            listOfClientSocketOnline.append(connectionSocket)
+            ClientMessageQueue[connectionSocket] = queue.Queue()
 
-            else:
+        # Read from the received socket
+        else:
+            # Can be an object or string
+            # Temporarily an object until we can add a diffrent thing ?
+            msgObject = socketTypesRead.recv(4096)
+            msgObjectDecoded = pickle.loads(msgObject)
+            # Print test the decoded object
+            print(msgObjectDecoded)
+
+            # Temp for testing - Add to connected client list if the message object type is HELLO
+            if msgObjectDecoded.msgType == 'HELLO':
+                print("HELLO MsgType")
+                # Add the new client id to list
+                listOfClientsOnlineId.append(msgObjectDecoded.senderId)
+                # Check the client
                 print(listOfClientsOnlineId)
+                # continue to avoid sending back
+                continue
+            # Temp for testing - set a chat target for the client if the message object type  is CHATSET
+            if msgObjectDecoded.msgType == 'CHATSET':
+                print("CHATSET MsgType")
+                print("Connect to")
+
+                # Check the client id and the online id list
+                print(msgObjectDecoded.targetId)
+                print(listOfClientsOnlineId)
+                # Assume that target is already online
+                # We can add a check if needed here to see if the client is busy ( due to being in a pair already)
+                # Can Check also if already connected
+                connectedPair.append(tuple((int(msgObjectDecoded.targetId), msgObjectDecoded.senderId)))
+                # continue to avoid sending back
+                continue
+            # Temp for testing - can be used to end threads if this used later
+            if msgObjectDecoded.msgBody == 'end talk':
+                print(msgObject)
+                # can try
+                    # close the socket of the ccurrent connection
+                    # close the other client socket
+                    # break
+                    # end client threads
+                socketTypesRead.send(bytes("end talk", 'utf-8'))
+
+            # Temp for testing - shut down the server and disconnect the clienst sockets
+            if msgObjectDecoded.msgBody == 'end server':
+                print(msgObject)
+                print(listOfClientSocketOnline)
+                # close all socket of the ccurrent connection
+                for connectionsOnline in listOfClientSocketOnline:
+                    # Inform the clients of the closure
+                    connectionsOnline.close()
+                # End all clients threads
+                # Can also try and end the following :
+                    # ClientMessageQueue[connectionSocket] clear the messages ?
+                    # end udp thread here later  ?
+                # Close the server socket
+                serverSocket.close()
+                print("Exiting server")
+                exit(0)
+
+            # Receiving a message object that is valid
+            if msgObject:  # might be none, incorrect
+                # added to the queue of the socket
+                ClientMessageQueue[socketTypesRead].put(msgObject)
+                # If the socket is not in potential_writes to send form later
+                if socketTypesRead not in potential_writes:
+                    potential_writes.append(socketTypesRead)
+            # none msgObject ?
+            else:
+                # Remove the socket from the potential_writes to avoid
+                if socketTypesRead in potential_writes:
+                    potential_writes.remove(socketTypesRead)
+                potential_readers.remove(socketTypesRead)
+                # Close the socket
+                socketTypesRead.close()
+                # Delete the client socket message
+                del ClientMessageQueue[socketTypesRead]
+
+    # For each socket that is writable and is in the select_ready_to_write
+    for socketTypesWrite in select_ready_to_write:
+        try:
+            # get the message from the client messages queue
+            msgOn = ClientMessageQueue[socketTypesWrite].get_nowait()
+        except queue.Empty:
+            # Remove the writable socket
+            potential_writes.remove(socketTypesWrite)
+
+        else:
+            # Temp - Print the message to test
+            print(listOfClientsOnlineId)
+            print(int(msgObjectDecoded.targetId))
+            print(connectedPair)
+            try:
                 print(int(msgObjectDecoded.targetId))
-                print(connectedPair)
-                try:
-                    print(int(msgObjectDecoded.targetId))
-                    print(int(msgObjectDecoded.senderId))
-                    print(listOfClientsOnlineId)
-                    print('=================')
-                    print(listOfClientsOnlineId.index(int(msgObjectDecoded.targetId)))
-                    potential_readers[1+(listOfClientsOnlineId.index(int(msgObjectDecoded.targetId)))].send(msgOn)
+                print(int(msgObjectDecoded.senderId))
+                print(listOfClientsOnlineId)
+                print('=================')
+                print(listOfClientsOnlineId.index(int(msgObjectDecoded.targetId)))
 
-                    print("potential_readers sent")
+                # Send the message to the target client socket
 
+                # This based on using the target client id to find the target client socket in the list and then send
+                # the message to it. Currently it depends on using client message object that contain the target id
+                # like the TCP message idea of having source and target address
+                #
+                # make it so only a single chat command start the client chat phase?
+                # add the idea of connected pairs to check for error ?
+                #
+                potential_readers[1 + (listOfClientsOnlineId.index(int(msgObjectDecoded.targetId)))].send(msgOn)
+                # Temp - check if the message passed
+                print("potential_readers sent")
 
-                except Exception as e:
-                    print("Exception e was raised")
-                    print(e)
-
-
-
-
+            # If any error occurs during writing target
+            except Exception as e:
+                print("Exception e was raised")
+                print(e)
